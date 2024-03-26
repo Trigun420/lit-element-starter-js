49de12fa5b6227cb48e85788df21d1b83e4f0d3a
Matthew Belcher <mattygb85@gmail.com>
2024-03-26 08:16:30 +0100

    updating chartsy object handling logic

--- rollup.config.js
+++ rollup.config.js
@@ -10,7 +10,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 
 export default {
-  input: 'chartsy.js',
+  input: 'chartsy2.js',
   output: {
     file: 'chartsy.bundled.js',
     format: 'esm',
